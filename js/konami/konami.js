d94e6651c02208d3dda9f5b084eeb9a5ee5b3a41
trollando <trollando.dev@gmail.com>
2016-07-11 22:18:19 +0200

    crazy (  Y  ) doc enhancement

--- js/konami/konami.js
+++ js/konami/konami.js
@@ -1,6 +1,6 @@
-((code,acc)=>window.onkeyup=e=>(acc=code[acc]==e.keyCode?acc+1:0)-code.length
+((c,i)=>window.onkeyup=e=>(i=c[i||0]==e.keyCode?i+1:0)-c.length
 ||(document.documentElement.innerHTML=`<style>html{
 background:url("http://www.gifff.in/img/background.gif")
-no-repeat center center fixed;background-size:cover;}</style>`)
-)([38,38,40,40,37,39,37,39,66,65])
-// ^^vv<><>ba
+no-repeat center center fixed;background-size:cover;}</style>`))
+([38,38,40,40,37,39,37,39,66,65])
+// ↑  ↑  ↓  ↓  ←  →  ←  →  b  a
