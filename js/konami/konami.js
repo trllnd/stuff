4d96b4cc24459ca0679106d8fe07f6439631b7bf
trollando <trollando.dev@gmail.com>
2016-07-13 00:44:47 +0200

    .

--- js/konami/konami.js
+++ js/konami/konami.js
@@ -1,3 +1,3 @@
 (i=>(window.onkeyup=e=>(i=[38,38,40,40,37,39,37,39,66,65][i]-e.keyCode?0:i+1)-10
-||(document.documentElement.innerHTML=`<style>html{background:url(http://www.gi\
-fff.in/img/background.gif)}</style>`)))(0)/*   ↑  ↑  ↓  ↓  ←  →  ←  →  b  a   */
+||(document.documentElement.innerHTML='<style>html{background:url(http://www.gi\
+fff.in/img/background.gif)}</style>')))(0)/*   ↑  ↑  ↓  ↓  ←  →  ←  →  b  a   */
