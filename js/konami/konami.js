4f88592e68b2c7f4b7d1a552499e752fece7021f
trollando <trollando.dev@gmail.com>
2016-07-13 00:41:54 +0200

    .

--- js/konami/konami.js
+++ js/konami/konami.js
@@ -1,6 +1,3 @@
-((c,i)=>window.onkeyup=e=>(i=c[i||0]==e.keyCode?i+1:0)-c.length
-||(document.documentElement.innerHTML=`<style>html{
-background:url("http://www.gifff.in/img/background.gif")
-no-repeat center center fixed;background-size:cover;}</style>`))
-([38,38,40,40,37,39,37,39,66,65])
-// ↑  ↑  ↓  ↓  ←  →  ←  →  b  a
+(i=>(window.onkeyup=e=>(i=[38,38,40,40,37,39,37,39,66,65][i]-e.keyCode?0:i+1)-10
+||(document.documentElement.innerHTML=`<style>html{background:url(http://www.gi\
+fff.in/img/background.gif)}</style>`)))(0)/*   ↑  ↑  ↓  ↓  ←  →  ←  →  b  a   */
