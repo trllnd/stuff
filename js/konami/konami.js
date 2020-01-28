72a6cfe935b1d2e63242d0572e62a22282c78ac9
trllnd <trollando.dev@gmail.com>
2020-01-28 12:15:23 +0100

    Update konami.js

--- js/konami/konami.js
+++ js/konami/konami.js
@@ -1,3 +1,2 @@
-(i=>(window.onkeyup=e=>(i=[38,38,40,40,37,39,37,39,66,65][i]-e.keyCode?0:i+1)-10
-||(document.documentElement.innerHTML='<style>html{background:url(http://www.gi\
-fff.in/img/background.gif)}</style>')))(0)/*   ↑  ↑  ↓  ↓  ←  →  ←  →  b  a   */
+Κ=λ=>(i=0,onkeyup=e=>(i=[/* ↑  ↑  ↓  ↓  ←  →  ←  →  b  a */
+38,38,40,40,37,39,37,39,66,65][i]-e.keyCode?0:i+1)-10||λ())
